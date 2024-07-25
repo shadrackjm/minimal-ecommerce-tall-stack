bddf760ebd5577ed907fd8fc98ee583457e81b59
shadrackjm <shadrackmballah80@gmail.com>
2024-07-25 13:19:47 +0300

    updated the landing page

--- resources/views/livewire/product-section.blade.php
+++ resources/views/livewire/product-section.blade.php
@@ -1,13 +1,17 @@
 <div class='px-10 md:px-20 sm:px-30'>
-         <!-- brand new  -->
+         <!-- Brand New  -->
         <h2 class='font-medium text-[20px] my-3'>Brand New</h2>
         <livewire:product-listing />
 
-        <!-- source codes  -->
+        <!-- Smartphones & laptops  -->
         <h2 class='font-medium text-[20px] my-3'>Smartphones & laptops</h2>
         <livewire:product-listing />
 
-        <!-- Icons  -->
+        <!-- Outfits  -->
         <h2 class='font-medium text-[20px] my-3'>Outfits</h2>
+        <livewire:product-listing />
+
+        <!-- Digital Products  -->
+        <h2 class='font-medium text-[20px] my-3'>Digital Products</h2>
         <livewire:product-listing />
     </div>
