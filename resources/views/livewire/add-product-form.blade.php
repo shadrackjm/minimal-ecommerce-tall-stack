fd51a836fb7e618bcae68ecedf7d9f3c863a0021
shadrackjm <shadrackmballah80@gmail.com>
2024-07-27 17:14:51 +0300

    updated-spinner color

--- resources/views/livewire/add-product-form.blade.php
+++ resources/views/livewire/add-product-form.blade.php
@@ -94,7 +94,7 @@
                 </div>
                 <!-- End Section -->
                 <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                    <div wire:loading class="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
+                    <div wire:loading class="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
                       <span class="sr-only">Loading...</span>
                     </div>  
                   Submit and Save
