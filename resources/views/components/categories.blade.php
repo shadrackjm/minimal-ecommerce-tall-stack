4443d2e88837d45c6eee6a8fc98d1984929f9c55
shadrackjm <shadrackmballah80@gmail.com>
2024-08-02 00:59:01 +0300

    updated the links of view all

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,4 +1,8 @@
-<a class="m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-    {{-- {!! $icon !!} --}}
-    {{ $categoryName }}
+<a
+    href="#"
+    wire:click.prevent="toggleCategory({{ $category_id }})"
+    class="m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700
+    @if(in_array($category_id, $selectedCategories)) bg-gray-300 dark:bg-neutral-700 @endif"
+>
+    {{ $category->name }}
 </a>
--- resources/views/components/navigation/view-all.blade.php
+++ resources/views/components/navigation/view-all.blade.php
@@ -1,6 +1,6 @@
 <div class="flex justify-between">
     <h2 class='font-medium text-[20px] my-3'>{{ $Category }}</h2>
-    <a wire:navigate class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="">
+    <a wire:navigate class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="/all/products">
         View all 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
